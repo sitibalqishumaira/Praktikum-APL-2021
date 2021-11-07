8029742b9fc3eecd1aaa3334d9f9c80da25d542e
Siti Balqis Humaira <81942670+sitibalqishumaira@users.noreply.github.com>
2021-11-07 13:02:06 +0800

    menampilkan suara asli dari bebek dan kucing

--- Tugas Adapter Pattern/Main.java
+++ Tugas Adapter Pattern/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+    	Bebek duck = new BebekKwek();
         Kucing milo = new KucingMeow();
 
-        // bebek KW bersuara seperti kucing
+        // membungkus kucing di dalam sebuah MeowAdapter sehingga bisa berperilaku seperti bebek KW
         Bebek meowAdapter = new MeowAdapter(milo);
-        System.out.println("Suara bebek KW jaman now :");
+
+        System.out.println("suara bebek duck:");
+        duck.kwek();
+        System.out.println();
+
+        System.out.println("suara kucing milo:");
+        milo.meow();
+        System.out.println();
+
+        // bebek KW bersuara seperti kucing
+        System.out.println("Suara bebek KW jaman now seperti kucing :");
         meowAdapter.kwek();
     }
 }
