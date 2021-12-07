6bdb25c69526971697f59dce5617e0b5cba2650f
Siti Balqis Humaira <81942670+sitibalqishumaira@users.noreply.github.com>
2021-12-07 18:44:23 +0800

    membuat interface BallWeightInfoAdapter.java

---
+++ UAS Praktikum APL/BallWeightInfoAdapter.java
@@ -0,0 +1,6 @@
+// Class Adapter untuk mencocokkan informasi satuan berat bola
+public interface BallWeightInfoAdapter {
+
+    // Method untuk mengembalikan berat bola dalam satuan kilogram
+    double getWeight();
+}
